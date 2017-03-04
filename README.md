703b776f5b0bd4799bc85fa578dbcb1bb52cfff3
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-04 18:22:43 -0500

    Add files needed for continuous integration.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Continuous Integration Concepts
+
--- average.go
+++
@@ -1,9 +0,0 @@
-package continuousIntegration
-
-func average(numbers []int) int {
-	var sum int
-	for index := 0; index < len(numbers); index++ {
-		sum += numbers[index]
-	}
-	return sum / len(numbers)
-}
