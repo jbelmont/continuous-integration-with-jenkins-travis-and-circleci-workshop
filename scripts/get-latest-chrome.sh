7acb4724657945333fb5323d946679b04df07b5a
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-03-10 21:04:46 -0500

    Add scripts folder and script entry for circle ci.

--- scripts/get-latest-chrome.sh
+++ scripts/get-latest-chrome.sh
@@ -5,4 +5,4 @@ set -ex
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update
-apt-get --only-upgrade install google-chrome-stable
+apt-get -y install google-chrome-stable
