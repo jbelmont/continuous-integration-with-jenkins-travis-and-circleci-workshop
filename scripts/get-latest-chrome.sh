2bf954508aebbf26e51ba57c6f88d9f4bec642ca
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-08-17 14:27:24 -0400

    Update docs

---
+++ scripts/get-latest-chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt-get update
+apt-get -y install google-chrome-stable
